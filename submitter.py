965b48e3e5fa3b61f8737a2960803637fa43f8ae
ocean1 <ocean.kuzuri@gmail.com>
2015-11-24 10:26:21 +0100

    separate submitter from webservice

--- submitter.py
+++ submitter.py
@@ -1,50 +1,93 @@
-from __future__ import absolute_import, print_function, unicode_literals
-
-from bottle import post, run, request, abort
+from importlib import import_module
 from config import config
-import re
-import logging
-
-from backend.mongodb import MongoBackend
-
-logging.basicConfig(
-    format='[%(asctime)s] %(message)s',
-    level=logging.DEBUG)
-
-log = logging.getLogger(__name__)
 
 
-# define a regex for flags
-flag_regex = config.get("flag_regex", "^\w{31}=$")
-service_regex = "^\w{32}$"
+class SubmitException(Exception):
+    """Exception generated when something VERY wrong
+    happened during submission, transmission of the flags
+    should be retried!"""
+    pass
 
-backend = MongoBackend()
+
+class SubmitterBase(object):
+
+    def submit(self, flags):
+        """ this function will submit the flags to the scoreboard
+        returns: a dictionary containing the list
+        of accepted/old/wrong flags"""
+        raise NotImplementedError()
 
 
-#  web interface here
-@post('/submit')
-def submit_flag():
-    team = request.forms.get('team')
-    service = request.forms.get('service')
-    flags = request.forms.getall('flags')
+class iCTFSubmitter(SubmitterBase):
 
-    if not flags or not team or not service:
-        # bad request
-        abort(400)
+    def __init__(self):
+        token = config.get("token")
+        email = config.get("email")
+        ictf = import_module('ictf')
 
-    try:
-        team = int(team)
-    except:
-        abort(400, "team should be a number!")
+        self.t = ictf.Team(token, email)
+        super(Submitter, self).__init__()
 
-    if not re.match(service_regex, service):
-        abort(400, "wrong format for service \w{32}")
+    def submit(self, flags):
+        results = self.t.submit_flag(flags)
+        accepted = []
+        wrong = []
 
-    backend.insert_flags(team, service, flags)
+        for r in zip(flags, results):
+            if r[1]:
+                accepted.append(r[0])
+            else:
+                wrong.append(r[0])
 
-if __name__ == "__main__":
-        run(
-            host='localhost',
-            port=8080,
-            reloader=True,
-            debug=config.get("debug", False))
+        return {
+            "accepted": accepted,
+            "wrong": wrong,
+            "expired": []  # the ictf interface won't tell us the difference
+        }
+
+
+class ruCTFeSubmitter(SubmitterBase):
+
+    def __init__(self):
+        pwn = import_module('pwn')
+        self.remote = pwn.remote
+        super(Submitter, self).__init__()
+
+    def submit(self, flags):
+        """ this function will submit the flags to the scoreboard"""
+
+        accepted = []
+        wrong = []
+        old = []
+        unsubmitted = list(flags)
+        try:
+            with self.remote("flags.e.ructf.org", 31337) as r:
+                r.read()
+
+                for flag in flags:
+                    r.send(flag + "\n")
+
+                    output = r.recv()
+                    if "Accepted" in output:
+                        accepted.append(flag)
+                    elif "Old" in output:
+                        old.append(flag)
+                    else:
+                        wrong.append(flag)
+
+                    unsubmitted.remove(flag)
+
+        except Exception:
+            raise SubmitException(
+                "an exception was met while submitting flags uh oh...")
+
+        return {
+            "accepted": accepted,
+            "wrong": wrong,
+            "old": old,
+            "unsubmitted": unsubmitted,
+        }
+
+
+# choose the submit function here :)
+Submitter = iCTFSubmitter
