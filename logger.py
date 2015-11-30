7125e89830ff154c88b0b14f22a12eb4548cc165
ocean1 <ocean.kuzuri@gmail.com>
2015-11-30 20:10:29 +0100

    removed circular import, added logging facility

--- logger.py
+++ logger.py
@@ -1,7 +1,46 @@
 import logging
+import getpass
+from datetime import datetime
+from socket import gethostname
+from backend.mongodb import MongoBackend
+
 
 logging.basicConfig(
     format='[%(asctime)s] %(message)s',
     level=logging.DEBUG)
 
 log = logging.getLogger(__name__)
+
+backend = MongoBackend
+
+
+class MongoFormatter(logging.Formatter):
+    def format(self, record):
+        """Format exception object as a string"""
+        data = record.__dict__.copy()
+
+        if record.args:
+            msg = record.msg % record.args
+        else:
+            msg = record.msg
+
+        data.update(
+            username=getpass.getuser(),
+            time=datetime.utcnow(),
+            host=gethostname(),
+            message=msg,
+            args=tuple(unicode(arg) for arg in record.args)
+        )
+        if 'exc_info' in data and data['exc_info']:
+            data['exc_info'] = self.formatException(data['exc_info'])
+        return data
+
+
+class MongodbHandler(logging.Handler):
+
+    def __init__(self):
+        self.formatter = MongoFormatter()
+
+    def emit(self, record):
+        msg = self.format(record)
+        backend.insert_logmsg(msg)
