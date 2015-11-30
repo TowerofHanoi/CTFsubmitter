a835af3fc135570f94157226b583d42acd3078f6
ocean1 <ocean.kuzuri@gmail.com>
2015-11-30 21:20:24 +0100

    initial stats

--- webservice.py
+++ webservice.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
-from bottle import post, run, request, abort
+from bottle import post, get, run, request, abort
 from config import config
 import re
+
+from ipaddress import ip_address
 
 from backend.mongodb import MongoBackend
 
@@ -14,12 +16,20 @@ service_regex = "^\w{0,32}$"
 backend = MongoBackend()
 
 
+@get('/stats')
+def stats():
+    return ""
+
+
 #  web interface here
 @post('/submit')
 def submit_flag():
+    name = request.forms.get('name')
     team = request.forms.get('team')
     service = request.forms.get('service')
     flags = request.forms.getall('flags')
+    ip = request.environ.get('REMOTE_ADDR').decode('utf-8')
+    ip = int(ip_address(ip))
 
     if not flags or not team or not service:
         # bad request
@@ -33,7 +43,9 @@ def submit_flag():
     if not re.match(service_regex, service):
         abort(400, "wrong format for service \w{32}")
 
-    backend.insert_flags(team, service, flags)
+    backend.insert_flags(
+            team, service, flags,
+            name, ip)
 
 if __name__ == "__main__":
 
