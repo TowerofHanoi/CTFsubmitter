922143cd5f137c139247d34855cf50a05db72dd3
ocean1 <ocean.kuzuri@gmail.com>
2015-12-01 14:36:36 +0100

    connection handling of websocket :)

--- static/ws.js
+++ static/ws.js
@@ -1,12 +1,36 @@
-if ("WebSocket" in window) {
+var retries = 0;
+
+function connectws(){
+    if ("WebSocket" in window) {
     // try connecting to tornado websocket interface
     var ws = new WebSocket("ws://" + window.location.hostname + ":8000/websocket");
-        ws.onopen = function() {
-            // Web Socket is connected. You can send data by send() method.
-            // ws.send("message to send");
+    $(".footer").css("background-color","#ff6600");
+    $(".footer").text("connecting...");
+
+    ws.onopen = function() {
+        $(".footer").css("background-color","#009900");
+        $(".footer").hide(1000)
+        // Web Socket is connected. You can send data by send() method.
+        // ws.send("message to send");
     };
     ws.onmessage = function (evt) { var received_msg = JSON.parse(evt.data); };
-    ws.onclose = function() { alert('websocket closed :(\n...try realoading page') };
-} else {
-    alert('no websocket :(')
+    ws.onclose = function() {
+        if (retries < 5){
+            setTimeout(connectws, 1000);
+            $(".footer").css("background-color","#ff6600");
+            $(".footer").text("connecting...");
+            $(".footer").show(10);
+            retries++;
+        }else{
+            $(".footer").css("background-color","#ff0000");
+            $(".footer").text("cannot connect to websocket :<");
+        }
+    };} else {
+        $(".footer").css("background-color","#ff0000");
+        $(".footer").text("no websockets :<");
+    }
 }
+
+$( document ).ready(function() {
+    connectws();
+});
