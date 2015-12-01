7060abcd1079ff4f25dea378b2696c3dd3be5f86
ocean1 <ocean.kuzuri@gmail.com>
2015-12-01 14:41:31 +0100

    initial structure of websocket stats tornado script

---
+++ stats.py
@@ -0,0 +1,53 @@
+from tornado import websocket, web, ioloop
+import json
+
+cl = []
+
+
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+
+class SocketHandler(websocket.WebSocketHandler):
+    def check_origin(self, origin):
+        return True
+
+    def open(self):
+        if self not in cl:
+            cl.append(self)
+
+    def on_close(self):
+        if self in cl:
+            cl.remove(self)
+
+
+class ApiHandler(web.RequestHandler):
+
+    @web.asynchronous
+    def get(self, *args):
+        self.finish()
+        id = self.get_argument("id")
+        value = self.get_argument("value")
+        data = {"id": id, "value": value}
+        data = json.dumps(data)
+        for c in cl:
+            c.write_message(data)
+
+    @web.asynchronous
+    def post(self):
+        pass
+
+
+app = web.Application([
+    (r'/stats', SocketHandler),
+    # (r'/', IndexHandler),
+    # (r'/api', ApiHandler),
+    # (r'/(favicon.ico)', web.StaticFileHandler, {'path': '../'}),
+    # (r'/(rest_api_example.png)', web.StaticFileHandler, {'path': './'}),
+])
+
+
+if __name__ == '__main__':
+    app.listen(8888)
+    ioloop.IOLoop.instance().start()
