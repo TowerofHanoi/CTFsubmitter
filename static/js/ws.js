021ec6129dd112504cdc772710c79ec973457376
ocean1 <ocean.kuzuri@gmail.com>
2015-12-03 16:42:39 +0100

    moved logs in a table

--- static/js/ws.js
+++ static/js/ws.js
@@ -9,26 +9,35 @@ function add_log(msg){
     };
 
     level = levels[msg['levelname']];
+
+    // append data to the DOM item
+
+    row = $("#loglist tr:first()");
+    alert(row);
+    // check if we have the same message preceding this
+
     $("#loglist").prepend(
-        '<li class="list-group-item list-group-item-' +
-        level + '">' + msg['time'] +
-        "<span class=\"logmsg\">" + msg['message'] + "</span>" + '</li>');
+        '<tr class="' + level + '"><td>'
+            + msg['time'] +
+        "</td><td><span class=\"logmsg\">" + msg['message'] + "</span>" + '</td></tr>');
+
+    $("#loglist tr:first()").data(msg);
 };
+
+function set_footer(txt, color){
+    $(".footer").css("background-color", color);
+    $(".footer").text(txt);
+}
 
 function connectws(){
     if ("WebSocket" in window) {
-    // try connecting to tornado websocket interface
     var ws = new WebSocket("ws://" + window.location.hostname + ":8888/websocket");
-    $(".footer").css("background-color","#ff6600");
-    $(".footer").text("connecting...");
+    set_footer("connecting...", "#ff6600");
 
     ws.onopen = function() {
         retries = 0;
-        $(".footer").css("background-color","#009900");
-        $(".footer").text("connected...");
+        set_footer("connected...", "#009900");
         $(".footer").fadeOut(1800)
-        // Web Socket is connected. You can send data by send() method.
-        // ws.send("message to send");
     };
     ws.onmessage = function (evt) {
         var msg = JSON.parse(evt.data);
@@ -36,27 +45,25 @@ function connectws(){
             add_log(msg);
         }
     };
+
+    ws.onerror = function(err) {
+        set_footer(err, "#ff6600")
+        $(".footer").fadeIn(500);
+    }
+
     ws.onclose = function() {
         if (retries <= 5){
             setTimeout(connectws, 1000);
-            $(".footer").css("background-color","#ff6600");
-            $(".footer").text("connecting...");
+            set_footer("connecting...","#ff6600");
             $(".footer").fadeIn(500);
             retries++;
         }else{
-            $(".footer").css("background-color","#ff0000");
-            $(".footer").text("cannot connect to websocket :<");
+            set_footer("cannot connect to websocket :<","#ff0000");
         }
     };} else {
-        $(".footer").css("background-color","#ff0000");
-        $(".footer").text("no websockets :<");
+        set_footer("no websockets :<","#ff0000");
     }
 
-    ws.onerror = function(err) {
-        $(".footer").css("background-color","#ff6600");
-        $(".footer").text(err);
-        $(".footer").fadeIn(500);
-    }
 }
 
 $( document ).ready(function() {
