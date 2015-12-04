893e8ee1188740a35869ae30befb716910221004
ocean1 <ocean.kuzuri@gmail.com>
2015-12-04 09:18:27 +0100

    various fixes + bulk write for stats

--- stat_service/stats.py
+++ stat_service/stats.py
@@ -1,6 +1,6 @@
 from tornado import websocket, web, ioloop, gen
-import datetime
-from database import logs
+from database import logs, stats
+from pymongo import ASCENDING
 
 from utils import date_encoder
 import json
@@ -32,10 +32,17 @@ class SocketHandler(websocket.WebSocketHandler):
 
         if self not in client_list:
             client_list.append(self)
-        cursor = logs.find()
+        cursor = logs.find().sort('$natural', ASCENDING).limit(20)
         while(yield cursor.fetch_next):
             r = cursor.next_object()
             r[u'msgtype'] = 'log'
+            self.write_message(json.dumps(
+                r, default=date_encoder.default))
+
+        cursor = stats.find()
+        while (yield cursor.fetch_next):
+            r = cursor.next_object()
+            r[u'msgtype'] = 'stats'
             self.write_message(json.dumps(
                 r, default=date_encoder.default))
 
@@ -55,7 +62,7 @@ def push_log():
     while True:
         if not cursor.alive:
             # While collection is empty, tailable cursor dies immediately
-            yield gen.Task(loop.add_timeout, datetime.timedelta(seconds=1))
+            yield gen.sleep(1)
             cursor = logs.find(tailable=True, await_data=True)
 
         if (yield cursor.fetch_next):
@@ -73,7 +80,17 @@ def push_stats():
     # unlike the log function we will have to poll
     # the db for updates, aggregating results
     while True:
-        yield gen.sleep(5)
+        yield gen.sleep(15)
+        cursor = stats.find()
+        while (yield cursor.fetch_next):
+            r = cursor.next_object()
+
+            r[u'msgtype'] = 'stats'
+            msg = json.dumps(
+                r, default=date_encoder.default)
+
+            for client in client_list:
+                    client.write_message(msg)
 
 
 if __name__ == '__main__':
