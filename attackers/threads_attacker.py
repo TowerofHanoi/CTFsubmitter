7326adddd39fb8e9d5cdb39affdbdc923e053336
ocean1 <ocean.kuzuri@gmail.com>
2015-12-04 17:57:05 +0100

    threads attacker ready

--- attackers/threads_attacker.py
+++ attackers/threads_attacker.py
@@ -8,60 +8,59 @@ _service = "service_name"
 _author = "ocean"
 _submitter_url = 'http://***REMOVED***/submit'
 
-global flags
-flags = []
 threads = []
-
-global submitted_flags
-submitted_flags = []
 
 q = Queue.Queue()
 
-flg_re = r"FLG\w{13}"
 
+class Attacker(self):
+    flg_re = r"FLG\w{13}"
 
-def attack(target):
-    """ your main attack routing goes here """
+    def exploit(target):
+        """ your main attack routing goes here """
 
-    # here attack the service and get the flags
-    print target
+        # here attack the service and get the flags
+        print target
 
-    flags = [
-        "FLG1234567890123",
-        "FLGABCDEFGHI0123"]
+        flags = [
+            "FLG1234567890123",
+            "FLGABCDEFGHI0123"]
 
-    submit_flags(flags, target)
-    return
+        submit_flags(flags, target)
+        return
 
+    def submit_flags(flags, target):
+        requests.post(
+            _submitter_url,
+            data={
+                "service": _service,
+                "team": target['team_name'],
+                "flags": flags,
+                "name": _author})
 
-def submit_flags(flags, target):
-    requests.post(
-        _submitter_url,
-        data={
-            "service": _service,
-            "team": target['team_name'],
-            "flags": flags,
-            "name": _author})
+    def attack():
+        t = Team("***REMOVED***", "***REMOVED***")
+
+        while(1):
+
+            t_info = t.get_tick_info()
+
+            # sleep until the next round
+            sleep(t_info['approximate_seconds_left'])
+
+            targets = t.get_targets(_service)
+
+            # ugly, spawn one thread for each target!
+            for t in targets:
+                # attack phase
+                print "attacking", t
+                th = threading.Thread(None, exploit, args=(t,))
+                threads.append(th)
+                th.start()
+
+            # maybe we should wait for threads to close
 
 
 if __name__ == "__main__":
-
-    current_tick = 0
-    t = Team("***REMOVED***", "***REMOVED***")
-
-    while(1):
-
-        t_info = t.get_tick_info()
-
-        # sleep until the next round
-        sleep(t_info['approximate_seconds_left'])
-
-        targets = t.get_targets(_service)
-
-        # ugly, spawn one thread for each target!
-        for t in targets:
-            # attack phase
-            print "attacking", t
-            th = threading.Thread(None, attack, args=(t,))
-            threads.append(th)
-            th.start()
+    a = Attacker()
+    a.attack()
